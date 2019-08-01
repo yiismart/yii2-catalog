907f170e20a19ccb9a1d316a9c900f32e4c79736
Dmitry Khlystov <dkhlystov@gmail.com>
2019-08-02 00:24:02 +1000

    Doc

--- models/Coupon.php
+++ models/Coupon.php
@@ -5,6 +5,16 @@ use smart\db\ActiveRecord;
 
 class Coupon extends ActiveRecord
 {
+    const TYPE_AMOUNT = 0;
+    const TYPE_PERCENT = 1;
+    const TYPE_FREE_DELIVERY = 2;
+
+    private static $_typeNames = [
+        self::TYPE_AMOUNT => 'Amount',
+        self::TYPE_PERCENT => 'Percent',
+        self::TYPE_FREE_DELIVERY => 'Free delivery',
+    ];
+
     /**
      * @inheritdoc
      */
@@ -13,11 +23,19 @@ class Coupon extends ActiveRecord
         return 'catalog_coupon';
     }
 
+    /**
+     * Available customers relation
+     * @return yii\db\ActiveQueryInterface
+     */
     public function getCustomers()
     {
         return $this->hasMany(CouponCustomer::className(), ['coupon_id' => 'id']);
     }
 
+    /**
+     * Available products relation
+     * @return yii\db\ActiveQueryInterface
+     */
     public function getProducts()
     {
         return $this->hasMany(CouponProducts::className(), ['coupon_id' => 'id']);
