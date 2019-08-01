a733a61eb06536f027288b8b26ea4300d6ddd337
Dmitry Khlystov <dkhlystov@gmail.com>
2019-08-01 15:37:08 +1000

    Currency model

--- models/Currency.php
+++ models/Currency.php
@@ -1,13 +1,10 @@
 <?php
-
 namespace smart\catalog\models;
 
-use yii\db\ActiveRecord;
-use yii\db\Expression;
+use smart\db\ActiveRecord;
 
 class Currency extends ActiveRecord
 {
-
     /**
      * @inheritdoc
      */
@@ -18,18 +15,24 @@ class Currency extends ActiveRecord
 
     /**
      * @inheritdoc
+     * Default values
+     */
+    public function __construct($config = [])
+    {
+        parent::__construct(array_replace([
+            'precision' => 2,
+            'default' => false,
+        ], $config));
+    }
+
+    /**
+     * @inheritdoc
      */
     public function afterSave($insert, $changedAttributes)
     {
-        // Product price value
-        if (array_key_exists('rate', $changedAttributes)) {
-            Product::updateAll(['priceValue' => new Expression('price * '. $this->rate)], ['currency_id' => $this->id]);
-        }
-
         // Default
         if (array_key_exists('default', $changedAttributes) && $this->default) {
             $this->updateAll(['default' => false], ['<>', 'id', $this->id]);
         }
     }
-
 }
