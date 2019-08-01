24693e4f8066f430dbf8d88e317e0336f2b0a301
Dmitry Khlystov <dkhlystov@gmail.com>
2019-08-01 16:13:44 +1000

    Customer model

---
+++ models/Customer.php
@@ -0,0 +1,26 @@
+<?php
+namespace smart\catalog\models;
+
+use smart\db\ActiveRecord;
+use smart\user\models\User;
+
+class Customer extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'catalog_customer';
+    }
+
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    public function getPriceType()
+    {
+        return $this->hasOne(PriceType::className(), ['id' => 'price_type_id']);
+    }
+}
