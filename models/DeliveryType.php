ce491ef0b751b4ae2c6b715e96b8703999567626
Dmitry Khlystov <dkhlystov@gmail.com>
2019-08-02 14:51:50 +1000

    Update delivery type model

--- models/DeliveryType.php
+++ models/DeliveryType.php
@@ -5,6 +5,16 @@ use smart\db\ActiveRecord;
 
 class DeliveryType extends ActiveRecord
 {
+    const TYPE_PICKUP = 0;
+    const TYPE_COURIER = 1;
+    const TYPE_CARRIER = 2;
+
+    private static $_typeNames = [
+        TYPE_PICKUP => 'Pickup',
+        TYPE_COURIER => 'Courier',
+        TYPE_CARRIER => 'Carrier',
+    ];
+
     /**
      * @inheritdoc
      */
