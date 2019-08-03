0152a9921016605869e158ea2f911dd5c2c5186e
Dmitry Khlystov <dkhlystov@gmail.com>
2019-08-03 13:44:12 +1000

    Update customer model

--- models/Customer.php
+++ models/Customer.php
@@ -6,6 +6,14 @@ use smart\user\models\User;
 
 class Customer extends ActiveRecord
 {
+    const TYPE_PRIVATE = 0;
+    const TYPE_LEGAL = 1;
+
+    private static $_typeNames = [
+        TYPE_PRIVATE => 'Private person',
+        TYPE_LEGAL => 'Legal entity',
+    ];
+
     /**
      * @inheritdoc
      */
@@ -14,16 +22,28 @@ class Customer extends ActiveRecord
         return 'catalog_customer';
     }
 
+    /**
+     * User relation
+     * @return yii\db\ActiveQueryInterface
+     */
     public function getUser()
     {
         return $this->hasOne(User::className(), ['id' => 'user_id']);
     }
 
+    /**
+     * Price type relation
+     * @return yii\db\ActiveQueryInterface
+     */
     public function getPriceType()
     {
         return $this->hasOne(PriceType::className(), ['id' => 'price_type_id']);
     }
 
+    /**
+     * Addresses relation
+     * @return yii\db\ActiveQueryInterface
+     */
     public function getAddresses()
     {
         return $this->hasMany(Address::className(), ['customer_id' => 'id']);
